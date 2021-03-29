fb979487068f42d64e308d54fea0e0cf063babcb
David Li <davidli012345@gmail.com>
2021-03-29 01:21:59 +0000

    updating cfg settings

--- stock_screener/tickers/v2.py
+++ stock_screener/tickers/v2.py
@@ -22,14 +22,24 @@ class TickerControllerV2:
             "https://raw.githubusercontent.com/FriendlyUser/cad_tickers_list/main/static/latest/stocks.csv"
         )
         tickers_config = cfg.get("tickers_config")
+        us_df = None
         if tickers_config != None:
             industries = tickers_config.get("industries")
             if industries != None:
                 ticker_df = ticker_df[ticker_df["industry"].isin(industries)]
 
+            us_tickers_url = tickers_config.get("us_tickers_url")
+            if us_tickers_url != None:
+                # same format as above
+                us_df =  pd.read_csv(us_tickers_url)
+
         # get symbols from tickers
         ytickers_series = ticker_df.apply(self.ex_to_yahoo_ex, axis=1)
         ytickers = ytickers_series.tolist()
+        if us_df != None:
+            us_ytickers_series = us_df.apply(self.ex_to_yahoo_ex, axis=1)
+            us_ytickers = us_ytickers_series.tolist()
+            ytickers = [*ytickers, us_ytickers]
         self.yf_tickers = ytickers
 
     def get_ytickers(self) -> list:
@@ -49,10 +59,15 @@ class TickerControllerV2:
         if exchange == "CSE":
             # strip :CNX from symbol
             ticker = ticker.replace(":CNX", "")
+        
+        if exchange in ["OTCPK", "NYSE", "NASDAQ", "NYE", "NCM", "NSM", "NGS"]:
+            ticker = ticker.replace(":US", "")
         # 1min, 5min, 15min, 30min, 60min, daily, weekly, monthly
         switcher = {"TSXV": "V", "TSX": "TO", "CSE": "CN"}
-        yahoo_ex = switcher.get(exchange, "TSXV")
-        return f"{ticker}.{yahoo_ex}"
+        yahoo_ex = switcher.get(exchange, None)
+        if yahoo_ex != None:
+            return f"{ticker}.{yahoo_ex}"
+        return ticker
 
 
 if __name__ == "__main__":
